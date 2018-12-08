ffc466d551b6a479f45dfcb59505b9cf16c42815
HP <HP@LAB001-15>
2018-12-07 22:15:31 -0200

    commit quase ninja

--- src/main/java/com/example/demo/model/Venda.java
+++ src/main/java/com/example/demo/model/Venda.java
@@ -5,15 +5,12 @@
  */
 package com.example.demo.model;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Collection;
 import javax.persistence.OneToMany;
 /**
